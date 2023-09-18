08963a95234135a2de18b52242b739b54beb6254
alainluthier <alainluthier@gmail.com>
2023-09-18 13:20:51 -0400

    week02

---
+++ scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName="Alain Rodrigo Ramos Catari";
+let currentYear = new Date().getFullYear();
+let profilePicture="./images/alain.jpg";
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML=`<strong>${fullName}</strong>`;
+yearElement.textContent=currentYear;
+imageElement.setAttribute("src",profilePicture)
+imageElement.setAttribute("alt",`Profile image of ${fullName}`)
+
+
+/* Step 5 - Array */
+
+let favoriteFood=['Pizza','Spaghetti','Rice & Beans'];
+foodElement.innerHTML+=`<br>${favoriteFood}`;
+let anotherFood='Apple Pie';
+favoriteFood.push(anotherFood);
+foodElement.innerHTML+=`<br>${favoriteFood}`;
+favoriteFood.shift()
+foodElement.innerHTML+=`<br>${favoriteFood}`;
+favoriteFood.pop()
+foodElement.innerHTML+=`<br>${favoriteFood}`;
+
+
+
